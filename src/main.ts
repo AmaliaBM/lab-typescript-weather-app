276d34950fa7ebb201d9fb7c9b4d79ffa1125eb7
amalia barrigas <amaliabarrigas@gmail.com>
2025-07-01 10:12:25 +0200

    Ejercicio terminado sin interaciones bonus

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
 // src/main.ts
+
+
+import { getLocation, getCurrentWeather, displayLocation, displayWeatherData } from './utils.ts';
+
+const form = document.getElementById("weather-form") as HTMLFormElement;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); //Evitar el comportamiento predeterminado de envío del formulario.
+
+  const locationInput = document.getElementById("location") as HTMLInputElement;
+  const locationName = locationInput.value;
+  locationInput.value = ""; // Clear the form
+
+  getLocation(locationName)
+    .then((response) => {
+      if(response.results){
+
+        // 
+        const location = response.results[0];
+
+        // 
+        displayLocation(location);
+
+        // 
+        return getCurrentWeather(location);
+      } else {
+        //
+        throw new Error('Location not found');
+      }
+    })
+    .then((weatherData) => {
+
+      //
+      displayWeatherData(weatherData);
+      
+    })
+    .catch((error) => {
+      console.log("Error getting weather data");
+      console.log(error);
+    });
+
+});
